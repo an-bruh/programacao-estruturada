ef98d18da2413db23de3a87c0dd177a843649dd0
an-bruh <brunoh.ancelani@gmail.com>
2022-11-07 23:21:41 -0300

    salary calculator program

---
+++ input-output-salary.c
@@ -0,0 +1,23 @@
+//ep1
+
+//Escreva um programa que leia o número de um funcionário,
+//seu número de horas trabalhadas, o valor que recebe por
+//hora e calcula o salário desse funcionário. A seguir,
+//mostre o número e o salário do funcionário, com duas casas decimais.
+
+#include <stdio.h>
+
+int main(){
+  int number, work_hours;
+  double hourly, salary;
+
+  scanf("%d %d", &number, &work_hours);
+  scanf("%lf", &hourly);
+
+  salary = work_hours * hourly;
+
+  printf("NUMBER = %d\n", number);
+  printf("SALARY = U$ %.2lf\n", salary);
+
+  return 0;
+}
